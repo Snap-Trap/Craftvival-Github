eac3edbb8e32faffb16986e0688131c48b6b6d6d
Luca Minnegal <Luca.minnegal@gmail.com>
2025-12-11 09:58:29 +0100

    Inventory ui start

--- Craftvival/Assets/Scripts/InventoryScripts/InventoryUI.cs
+++ Craftvival/Assets/Scripts/InventoryScripts/InventoryUI.cs
@@ -1,16 +1,42 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //Creator: Luca
+
+    // Simple toggle for the inventory UI, needs the Canvas to work
+    public InputAction toggleAction;
+    public Canvas inventoryCanvas;
+
+    public List<GameObject> inventorySlots = new List<GameObject>();
+
+    public void Start()
     {
-        
+        inventoryCanvas = GetComponent<Canvas>();
+
+        inventoryCanvas.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        // If toggleAction is pressed, toggle the inventory UI
+        if (toggleAction.ReadValue<float>() == 1)
+        {
+            inventoryCanvas.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnEnable()
+    {
+        toggleAction.Enable();
+    }
+
+    public void OnDisable()
+    {
+        toggleAction.Disable();
     }
 }
