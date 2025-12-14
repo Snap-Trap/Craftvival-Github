2096a5a07c454343fc157316b5ce14db966b1759
SnapTrap <Luca.minnegal@gmail.com>
2025-12-14 22:57:41 +0100

    Revert "Added Scared enemy that runs away when hit"
    This reverts commit ddaefec67ec50bb2b01f67d75f2f05d7f4727321.

--- Craftvival/Assets/Scripts/EntityScripts/BaseEntity.cs
+++ Craftvival/Assets/Scripts/EntityScripts/BaseEntity.cs
@@ -8,7 +8,6 @@ public class BaseEntity : MonoBehaviour, IDamagable
 
     // Basic variables
     public float health;
-    public bool isDamaged;
 
     public void Awake()
     {
@@ -19,24 +18,6 @@ public class BaseEntity : MonoBehaviour, IDamagable
     public void TakeDamage(float damage)
     {
         health -= damage;
-<<<<<<< Updated upstream
-=======
-        StartCoroutine(DamagedTimer());
-
-        if (health <= 0)
-        {
-            Die();
-        }
->>>>>>> Stashed changes
-    }
-
-    private IEnumerator DamagedTimer()
-    {
-        isDamaged = true;
-        Debug.Log(gameObject.name + " has been damaged");
-        yield return new WaitForSeconds(15);
-        isDamaged = false;
-        Debug.Log(gameObject.name + " is no longer damaged");
     }
 
     public void Die()
