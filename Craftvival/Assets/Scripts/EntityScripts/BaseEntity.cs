236b4213b13b771710a17b5114ec20edb686ed5a
SnapTrap <Luca.minnegal@gmail.com>
2025-12-15 00:16:29 +0100

    Revert "Added player melee attack + updated TakeDamage functies for the BaseEntity script"
    This reverts commit bbe291a78d21a0ed8fbbf6920a04338ea535942f.

--- Craftvival/Assets/Scripts/EntityScripts/BaseEntity.cs
+++ Craftvival/Assets/Scripts/EntityScripts/BaseEntity.cs
@@ -18,15 +18,13 @@ public class BaseEntity : MonoBehaviour, IDamagable
     public void TakeDamage(float damage)
     {
         health -= damage;
-
-        if (health <= 0)
-        {
-            Die();
-        }
     }
 
     public void Die()
     {
-        Destroy(gameObject);
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
