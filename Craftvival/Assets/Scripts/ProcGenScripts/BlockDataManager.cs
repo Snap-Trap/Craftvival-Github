72b6761a6b5ed7fd543ab49b217c072d3acd8bc8
SnapTrap <Luca.minnegal@gmail.com>
2026-01-11 00:29:55 +0100

    Added BlockManager for the sprites

---
+++ Craftvival/Assets/Scripts/ProcGenScripts/BlockDataManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BlockDataManager : MonoBehaviour
+{
+    public static float textureOffset = 0.001f;
+    public static float tileSizeX, tileSizeY;
+    public static Dictionary<BlockType, TextureData> blockTextureDataDictionary = new Dictionary<BlockType, TextureData>();
+    public BlockDataSO textureData;
+
+    private void Awake()
+    {
+        foreach (var item in textureData.textureDataList)
+        {
+            if (blockTextureDataDictionary.ContainsKey(item.blockType) == false)
+            {
+                blockTextureDataDictionary.Add(item.blockType, item);
+            }
+        }
+        tileSizeX = textureData.textureSizeX;
+        tileSizeY = textureData.textureSizeY;
+    }
+}
--- Craftvival/Assets/Scripts/ProcGenScripts/BlockDataSO.cs
+++ Craftvival/Assets/Scripts/ProcGenScripts/BlockDataSO.cs
@@ -1,7 +1,19 @@
+using System;
 using UnityEngine;
+using System.Collections.Generic;
 
-[CreateAssetMenu(fileName = "BlockDataSO", menuName = "Scriptable Objects/BlockDataSO")]
+[CreateAssetMenu(fileName = "Block Data", menuName = "Data/Block Data")]
 public class BlockDataSO : ScriptableObject
 {
-    
+    public float textureSizeX, textureSizeY;
+    public List<TextureData> textureDataList;
+}
+
+[Serializable]
+public class TextureData
+{
+    public BlockType blockType;
+    public Vector2Int up, down, side;
+    public bool isSolid = true;
+    public bool generatesCollider = true;
 }
