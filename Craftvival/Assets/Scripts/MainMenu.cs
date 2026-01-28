5d1ce8cdce599990abb91bc8d5a494e589bcbf93
Luca Minnegal <Luca.minnegal@gmail.com>
2026-01-28 11:23:38 +0100

    Material update + MainScene layout

--- Craftvival/Assets/Scripts/MainMenu.cs
+++ Craftvival/Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     // Script made by Charly
     public void StartGame()
     {
-        SceneManager.LoadScene("CharlyScene");// Loads the scene of the main game
+        SceneManager.LoadScene("MainScene");// Loads the scene of the main game
     }
 
     public void ExitGame()
